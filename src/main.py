4dbac9a69f61284ce0127ad7777a3a30e2034702
Noel Joby <noeljjoby@gmail.com>
2023-02-04 13:11:45 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -1,22 +1,16 @@
-from topic_iterator import TopicIterator
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
 import sys
 from datetime import datetime
 
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+
+from topic_iterator import TopicIterator
 
 SYSTEMFONT: str = "Helvetica"
-
-
-class Colour(QWidget):
-    def __init__(self, color) -> None:
-        super(Colour, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette: QPalette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(color))
-        self.setPalette(palette)
+NAME: str = "noel"
+CALENDER: str = "GCSEs"
 
 
 class MainWindow(QMainWindow):
@@ -25,41 +19,48 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Retrospective Calender App")
         self.setMinimumSize(600, 500)
 
+        self.scheduler: TopicIterator = TopicIterator(NAME, CALENDER)
+
+        left_col_layout: QVBoxLayout = QVBoxLayout()
+        for topic_label in self.create_left_col():
+            left_col_layout.addWidget(topic_label)
+        left_col_cont: QWidget = QWidget()
+        left_col_cont.setLayout(left_col_layout)
+
+        self.glayout: QGridLayout = QGridLayout()
+        self.glayout.addWidget(self.create_date_box(), 0, 0)
+        self.glayout.addWidget(left_col_cont, 1, 0)
+
+        container: QWidget = QWidget()
+        container.setLayout(self.glayout)
+        self.setCentralWidget(container)
+
+    def create_date_box(self) -> QLabel:
         today: datetime = datetime.now()
         date_box: QLabel = QLabel(today.strftime("%A %d\n%B\n%Y"), self)
         date_box.setFont(QFont(SYSTEMFONT, 16, 2, False))
         date_box.adjustSize()
+        return date_box
 
-        layout: QGridLayout = QGridLayout()
-        layout.addWidget(date_box, 0, 0)
+    def create_left_col(self) -> list[QLabel]:
+        left_col: list[QLabel] = []
 
-        container: QWidget = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        for i in range(9):
+            name: str | None = self.scheduler.get_topic_name(i)
+            score: float | None = self.scheduler.get_topic_score(i)
+
+            if name is None:
+                break
+            text: str = f"{name.capitalize()} --- {score}"
+
+            label_box: QLabel = QLabel(text)
+            label_box.setFont(QFont(SYSTEMFONT, 16, 1, False))
+            label_box.adjustSize()
+            left_col.append(label_box)
+        return left_col
 
 
 App: QApplication = QApplication(sys.argv)
-
 main_win: MainWindow = MainWindow()
-
 main_win.show()
 App.exec()
-
-
-# reset code
-# import pickle
-# from topic_class import Topic
-# with open("../data/noel/GCSEs/topics.pkl", "wb") as f:
-#     pickle.dump([], f)
-
-
-# x: TopicIterator = TopicIterator("noel", "GCSEs")
-
-# x.add_new_topics()
-# x.schedule_topics()
-# for topic in x:
-#     print(topic)
-#     input("Press enter to display next topic")
-
-# input("close [ENTER]")
-# x.close_scheduler()
--- src/topic_iterator.py
+++ src/topic_iterator.py
@@ -10,6 +10,9 @@ class TopicIterator():
         with open(f"../data/{name}/{calender}/topics.pkl", "rb") as f:
             self.topics = pickle.load(f)
 
+        self.name = name
+        self.calender = calender
+
     def __iter__(self) -> Self:
         self.pos: int = -1
         return self
@@ -20,6 +23,10 @@ class TopicIterator():
         at each iteration calls that topic's revise func.
         """
         self.topics.sort(key=lambda x: x.score)
+        if len(self.topics) == 0:
+            print("No topics")
+            raise StopIteration
+
         if self.pos == len(self.topics) - 1:  # 2
             self.topics[self.pos].revise(
                 float(input("New Difficulty 1-10 (10=easy):\n")))
@@ -31,6 +38,12 @@ class TopicIterator():
 
         self.pos += 1
         return self.topics[self.pos]
+
+    def get_topic_name(self, pos: int) -> str | None:
+        return None if pos > len(self.topics)-1 else self.topics[pos].name
+
+    def get_topic_score(self, pos: int) -> float | None:
+        return None if pos > len(self.topics)-1 else self.topics[pos].score
 
     def add_new_topics(self) -> None:
         # add new topics
@@ -50,5 +63,27 @@ class TopicIterator():
         self.topics.sort(key=lambda x: x.score)
 
     def close_scheduler(self) -> None:
-        with open("../data.pkl", "wb") as f:
+        with open(f"../data/{self.name}/{self.calender}/topics.pkl", "wb") as f:
             pickle.dump(self.topics, f)
+
+# reset code
+# import pickle
+# from topic_class import Topic
+# with open("../data/noel/GCSEs/topics.pkl", "wb") as f:
+#     pickle.dump([], f)
+
+
+if __name__ == "__main__":
+    x: TopicIterator = TopicIterator("noel", "GCSEs")
+
+    x.add_new_topics()
+    x.schedule_topics()
+    cont: bool = input("Enter Y to display first topic [Y/N]") == "Y"
+    for topic in x:
+        cont = input("Enter Y to display next topic [Y/N]") == "Y"
+        if not (cont):
+            break
+        print(topic)
+
+    input("close [ENTER]")
+    x.close_scheduler()
